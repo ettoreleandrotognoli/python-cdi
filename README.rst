5ebd7f81beeb230f1b1bda6ff3a26f37cfed69bb
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2017-01-24 02:29:18 -0200

    repo config

---
+++ README.rst
@@ -0,0 +1,16 @@
+=====
+PyCDI
+=====
+
+
+A simple Python CDI ( Code Dependency Injection) Library.
+
+Install
+-------
+
+Install pycdi
+
+.. code-block:: shell
+
+    pip install pycdi
+
---
+++ setup.py
@@ -0,0 +1,37 @@
+# -*- encoding: utf-8 -*-
+__author__ = 'ettore'
+
+import os
+
+from setuptools import setup, find_packages
+
+from pycdi import __version__
+
+
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+
+requirements = []
+str_version = ".".join(map(str, __version__))
+
+setup(
+    name="pycdi",
+    version=str_version,
+    description="Python Code Dependency Injection",
+    long_description=read('README.rst'),
+    url='https://github.com/ettoreleandrotognoli/python-cdi',
+    download_url='https://github.com/ettoreleandrotognoli/python-cdi/tree/%s/' % str_version,
+    license='BSD',
+    author=u'Éttore Leandro Tognoli',
+    author_email='ettore.leandro.tognoli@gmail.com',
+    packages=find_packages(exclude=['tests','examples']),
+    include_package_data=True,
+    keywords=['cdi','di','code dependency injection','dependency injection'],
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    # install_requires=requirements,
+    # tests_require=[],
+)
