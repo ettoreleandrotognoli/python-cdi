db45b8746150a82e4045c5a568968d4027b26100
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2018-04-24 00:05:08 -0300

    fix tests

--- tests/common/test_injection_point.py
+++ tests/common/test_injection_point.py
@@ -1,11 +1,9 @@
 # -*- encoding: utf-8 -*-
 import unittest
-
-from pycdi.core import PyCDIContainer
-from pycdi.shortcuts import call
 from random import random, choice
+
 from pycdi import Producer, Inject
-from pycdi.core import InjectionPoint
+from pycdi.core import InjectionPoint, PyCDIContainer
 
 
 class InjectionPointTest(unittest.TestCase):
@@ -13,7 +11,9 @@ class InjectionPointTest(unittest.TestCase):
     str_injection_point = None
 
     def test_injection_points(self):
-        @Producer(produce_type=float)
+        container = PyCDIContainer()
+
+        @Producer(produce_type=float, _container=container)
         @Inject(injection_point=InjectionPoint)
         def float_producer(injection_point):
             self.assertEqual(injection_point.type, float)
@@ -23,7 +23,7 @@ class InjectionPointTest(unittest.TestCase):
             self.float_injection_point = injection_point
             return random()
 
-        @Producer(produce_type=str)
+        @Producer(produce_type=str, _container=container)
         @Inject(injection_point=InjectionPoint)
         def float_producer(injection_point):
             self.assertEqual(injection_point.type, str)
@@ -38,13 +38,13 @@ class InjectionPointTest(unittest.TestCase):
         def receive_float(random_float):
             self.assertIsNotNone(random_float)
 
-        call(receive_float)
+        container.call(receive_float)
 
         @Inject(random_str=str)
         def receive_str(random_str):
             self.assertIsNotNone(random_str)
 
-        call(receive_str)
+        container.call(receive_str)
 
         self.assertIsNotNone(self.float_injection_point)
         self.assertIsNotNone(self.str_injection_point)
