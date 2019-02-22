6d9c751f38a011b9041ba48108a1ff91f4382df0
Éttore Leandro Tognoli <ettore.leandro.tognoli@gmail.com>
2019-02-22 19:36:35 -0300

    producer in service not work

--- tests/common/test_service.py
+++ tests/common/test_service.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from pycdi.core import Service, Inject
+from pycdi.core import Service, Inject, Producer
 from pycdi.shortcuts import call
 
 
@@ -11,6 +11,18 @@ class AnyService(object):
         pass
 
 
+@Service()
+class ServiceWithProducers(object):
+
+    def __init__(self):
+        self.counter = 0
+
+    @Producer(int)
+    def next_int(self):
+        self.counter += 1
+        return self.counter
+
+
 class ServiceTest(TestCase):
 
     def test_inject_service(self):
@@ -19,3 +31,15 @@ class ServiceTest(TestCase):
             self.assertIsInstance(service, AnyService)
 
         call(receive_service)
+
+    def test_inject_from_service_method(self):
+        expected = 1
+
+        @Inject(number=int)
+        def receive_service_product(number):
+            self.assertIsInstance(number, int)
+            self.assertEqual(number, expected)
+
+        call(receive_service_product)
+        expected = 2
+        call(receive_service_product)
